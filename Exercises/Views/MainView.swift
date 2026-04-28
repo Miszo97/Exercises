ac3d1a9c15abf2594eb01826ff9105f9afd1b4ba
artur <artur.spek@gmail.com>
2026-04-28 21:59:40 +0200

    Remove surplus exercises from MainView initialization for simplicity

--- Exercises/Views/MainView.swift
+++ Exercises/Views/MainView.swift
@@ -63,26 +63,6 @@ struct MainView: View {
             AddableExercise(name: "push ups", type: .reps, valueToAdd: 20),
             AddableExercise(name: "plank", type: .duration, valueToAdd: 60),
             AddableExercise(name: "squats", type: .reps, valueToAdd: 30),
-            AddableExercise(name: "jumping jacks", type: .reps, valueToAdd: 40),
-            AddableExercise(name: "burpees", type: .reps, valueToAdd: 10),
-            AddableExercise(name: "lunges", type: .reps, valueToAdd: 20),
-            AddableExercise(name: "bicycle crunches", type: .reps, valueToAdd: 15),
-            AddableExercise(name: "mountain climbers", type: .reps, valueToAdd: 50),
-            AddableExercise(name: "sit ups", type: .reps, valueToAdd: 25),
-            AddableExercise(name: "high knees", type: .duration, valueToAdd: 30),
-            AddableExercise(name: "leg raises", type: .reps, valueToAdd: 20),
-            AddableExercise(name: "tricep dips", type: .reps, valueToAdd: 15),
-            AddableExercise(name: "calf raises", type: .reps, valueToAdd: 30),
-            AddableExercise(name: "wall sit", type: .duration, valueToAdd: 45),
-            AddableExercise(name: "heel touches", type: .reps, valueToAdd: 20),
-            AddableExercise(name: "superman", type: .duration, valueToAdd: 60),
-            AddableExercise(name: "flutter kicks", type: .duration, valueToAdd: 45),
-            AddableExercise(name: "side lunges", type: .reps, valueToAdd: 20),
-            AddableExercise(name: "shoulder taps", type: .reps, valueToAdd: 30),
-            AddableExercise(name: "skaters", type: .reps, valueToAdd: 30),
-            AddableExercise(name: "reverse lunges", type: .reps, valueToAdd: 20),
-            AddableExercise(name: "butt kicks", type: .duration, valueToAdd: 30),
-            AddableExercise(name: "side planks", type: .duration, valueToAdd: 30)
         ]
     )
 }
